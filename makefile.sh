74d11b140e2ca0d315d445d56e1fbfcab07b8a9b
piotrowy <petr.ceranek@gmail.com>
2016-04-17 23:38:10 +0200

    generowanie dokumentacji w makefile

--- makefile.sh
+++ makefile.sh
@@ -11,7 +11,11 @@ then
       echo "Sails already installed"
   fi
 fi
+echo "Instalowanie zaleznosci"
 npm install
 cp config/connections.default.js config/connections.js
+echo "Genereowanie dokumentacji"
+grunt jsdoc
+echo "Odpalamy"
 sails lift
 
